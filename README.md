278571f50b91e5735f213d1da03203433ed9f018
Eetu Tuomala <eetu.tuomala@maas.global>
2018-08-24 16:52:14 +0300

    serverless build plugin refactor

--- README.md
+++ README.md
@@ -1,40 +1,39 @@
-# Serverless Ansible Build Plugin
+# Serverless CI Build Plugin
 
-
-Plugin hooks to deploy command and is executed with `--ansible` flog.
+Plugin hooks to deploy command and is executed with `--buildPlugin` flog.
 
 ```
-sls deploy --ansible
+sls deploy --buildPlugin
 ```
 
 serverless.yml
 ```
 plugins:
-  - serverless-ansible-build-plugin
+  - serverless-plugin-ci-build
 ```
 
-By default region is replaced with `{{ region }}`, stage with `{{ stage }}` and artifact path `{{ artifact_path }}`. Build directory, where the j2 template and zip is created is `.ansible`.
+By default region is replaced with `{{ region }}`, stage with `{{ stage }}` and artifact path `{{ artifact_path }}`. Build directory, where the j2 template and zip is created is `.package`.
 
 To change to custom values, those can be defined in `custom` block.
- 
+
 ```
 custom:
-  ansible:
-    buildDirectory: ".ansible"
-    region: "{{ region }}"
-    stage: "{{ stage }}"
-    artifactPath: "{{ artifact_path }}"
+  buildPlugin:
+    buildDirectory: ".package"
+    region: "{{region}"
+    stage: "{{stage}}"
+    artifactPath: "{{artifact_path}}"
     environment:
-      SECRET: "{{ secret }}"
+      SECRET: "{{secret}}"
 
 ```
 
-To overwrite Lambda environmental variables defined in Serverless service provider, one option is to define those in custom ansible environment and use in provider environment:
+To overwrite Lambda environmental variables defined in Serverless service provider, one option is to define those in custom buildPlugin environment and use in provider environment:
 
 ```
 provider:
   name: aws
   runtime: nodejs4.3
   environment:
-    SECRET: ${env:SECRET, self:custom.ansible.environment.SECRET}
+    SECRET: ${env:SECRET, self:custom.buildPlugin.environment.SECRET}
 ```
--- index.js
+++ index.js
@@ -5,45 +5,33 @@ const path = require('path');
 const uuidV4 = require('uuid/v4');
 const chalk = require('chalk');
 
-class AnsibleBuild {
+class CIBuildPlugin {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
     this.uuidRegion = uuidV4();
     this.uuidStage = uuidV4();
-    this.ansible = this.serverless.service.custom ? this.serverless.service.custom.ansible : {};
+    this.buildPlugin = this.serverless.service.custom ? this.serverless.service.custom.buildPlugin : {};
 
     // @todo if deployment bucket is set it should be removed when deploying with noDeploy
     // this.uuidDeploymentBucket = uuidV4();
 
-    if (this.options.ansible) {
+    if (this.options.buildPlugin) {
       this.options = Object.assign(this.options,
         {
           noDeploy: true,
-          stage: this.ansible.templateStage || this.uuidStage,
-          region: this.ansible.templateRegion || this.uuidRegion,
+          stage: this.buildPlugin.templateStage || this.uuidStage,
+          region: this.buildPlugin.templateRegion || this.uuidRegion,
         });
     }
-
-    // @todo create sls build ansible command
-    this.commands = {
-      // build: {
-      //   commands: {
-      //     ansible: {
-      //       usage: 'Builds ansible j2 template',
-      //       lifecycleEvents: ['build'],
-      //     },
-      //   },
-      // },
-    };
-
+    this.commands = {};
     this.hooks = {
       'after:deploy:deploy': this.createArtifacts.bind(this),
     };
   }
 
   createArtifacts() {
-    if (this.options.ansible) {
+    if (this.options.buildPlugin) {
       const stack =
         JSON.parse(fse.readFileSync(path.join('.serverless', 'cloudformation-template-update-stack.json'), 'utf8'));
 
@@ -64,29 +52,29 @@ class AnsibleBuild {
 
       const replacer = (key, value) => {
         if (typeof (value) === 'string') {
-          const ansibleStage = this.ansible.stage || '{{ stage }}';
-          const ansibleRegion = this.ansible.region || '{{ region }}';
-          const ansibleArtifact = this.ansible.artifactPath || '{{ artifact_path }}';
+          const buildPluginStage = this.buildPlugin.stage || '{{ stage }}';
+          const buildPluginRegion = this.buildPlugin.region || '{{ region }}';
+          const buildPluginArtifact = this.buildPlugin.artifactPath || '{{ artifact_path }}';
           const regexStage = new RegExp(this.options.stage, 'g');
           const regexRegion = new RegExp(this.options.region, 'g');
-          const regexArtifact = new RegExp(`serverless/${serviceName}/${ansibleStage}/[0-9-T:.Z]+/`, 'g');
-          return value.replace(regexStage, ansibleStage)
-            .replace(regexRegion, ansibleRegion)
-            .replace(regexArtifact, `${ansibleArtifact}/`);
+          const regexArtifact = new RegExp(`serverless/${serviceName}/${buildPluginStage}/[0-9-T:.Z]+/`, 'g');
+          return value.replace(regexStage, buildPluginStage)
+            .replace(regexRegion, buildPluginRegion)
+            .replace(regexArtifact, `${buildPluginArtifact}/`);
         }
         return value;
       };
 
-      const ansibleDir = this.ansible.buildDirectory || '.ansible';
-      const templatePath = path.join(ansibleDir, `${serviceName}.json.j2`);
+      const buildPluginDir = this.buildPlugin.buildDirectory || '.buildPlugin';
+      const templatePath = path.join(buildPluginDir, `${serviceName}.json.j2`);
 
-      // Create ansible deployment directory
-      fse.mkdirsSync(ansibleDir);
+      // Create buildPlugin deployment directory
+      fse.mkdirsSync(buildPluginDir);
 
       // Save template
       const template = JSON.stringify(stack, replacer, 2);
       fse.writeFileSync(templatePath, template);
-      this.log(`Created ansible template ${templatePath}`);
+      this.log(`Created buildPlugin template ${templatePath}`);
 
       Object.keys(this.serverless.service.functions).reduce((result, key) => {
         const artifact = this.serverless.service.functions[key].package.artifact;
@@ -100,15 +88,15 @@ class AnsibleBuild {
           // Copy zip
           fse.copySync(
             zipfile,
-            path.join(ansibleDir, zipfilename));
-          this.log(`Copied zip ${zipfilename} to ${ansibleDir}/${zipfilename}`);
+            path.join(buildPluginDir, zipfilename));
+          this.log(`Copied zip ${zipfilename} to ${buildPluginDir}/${zipfilename}`);
         });
     }
   }
 
   log(message) {
-    this.serverless.cli.consoleLog(`Serverless Ansible Build Plugin: ${chalk.yellow(message)}`);
+    this.serverless.cli.consoleLog(`Serverless Plugin CI Build: ${chalk.yellow(message)}`);
   }
 }
 
-module.exports = AnsibleBuild;
+module.exports = CIBuildPlugin;
